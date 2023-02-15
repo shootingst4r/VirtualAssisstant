3de92b942efbb975bd442738935878d63875fd3e
shootingst4r <121058591+shootingst4r@users.noreply.github.com>
2023-02-15 11:01:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import pyttsx3
 import speech_recognition as sr
 import webbrowser
 
+#start TTS engine
 engine = pyttsx3.init()
 
+#start SpeechRecognition Engine
 mic = sr.Microphone()
 r = sr.Recognizer()
 
 firefox_path = r"C:\Program Files\Mozilla Firefox\firefox.exe"
-
 webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(firefox_path))
 
 with mic as source:
@@ -17,9 +18,19 @@ with mic as source:
 
 result = r.recognize_google(audio)
 checkresult = result.lower()
+
+#To add more use this scheme []
+#
+#if checkresult.find("[name]") != -1:
+#
+#if you want to open a browser use this:
+#       webbrowser.get('[BROWSER]').open("[website URL]")
+#
+#if you want a message to be said use this:
+#	engine.say("[message]")
+#	engine.runAndWait()
+
 if checkresult.find("youtube") != -1:
-	webbrowser.get('firefox').open("youtube.com")
+        webbrowser.get('firefox').open("youtube.com")
 	engine.say("Youtube started")
 	engine.runAndWait()
-if checkresult.find("duckduckgo") != -1:
-  webbrowser.get('firefox').open("duckduckgo.com")
